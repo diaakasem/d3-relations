4a352230a9d804681f3c7c0dfbfae1b69659bc4d
Diaa Kasem <me@diaa.me>
2014-06-01 18:06:44 +0200

    Few fixes

--- first/app/js/main.js
+++ first/app/js/main.js
@@ -50,6 +50,7 @@ $(document).ready(function() {
             compiledData = {nodes: nodes, links: links};
 
             $('#dendrogram a').click(function() {
+                configs.height = 500;
                 $('#chart *').remove();
                 Dendrogram(_.cloneDeep(compiledData), configs);
             });
@@ -64,6 +65,7 @@ $(document).ready(function() {
                 Hive(_.cloneDeep(compiledData), configs);
             });
             $('#matrix a').click(function() {
+                configs.height = 800;
                 $('#chart *').remove();
                 Matrix(_.cloneDeep(compiledData), configs);
             });
